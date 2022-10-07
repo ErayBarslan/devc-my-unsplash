ab46b0640443d1ce621529c8b8e16f017716e293
Eray <eraybarslan@gmail.com>
2022-10-07 18:53:18 +0300

    api source

--- client/next.config.js
+++ client/next.config.js
@@ -4,19 +4,19 @@ const rewrites = async () => {
   return [
     {
       source: "/photos",
-      destination: `http://localhost:5000/api/${process.env.API_KEY}/photos`,
+      destination: `https://devc-my-unsplash-api.onrender.com/api/${process.env.API_KEY}/photos`,
     },
     {
       source: "/photos/delete/:id",
-      destination: `http://localhost:5000/api/${process.env.API_KEY}/photos/delete/:id`,
+      destination: `https://devc-my-unsplash-api.onrender.com/api/${process.env.API_KEY}/photos/delete/:id`,
     },
     {
       source: "/photos/:page",
-      destination: `http://localhost:5000/api/${process.env.API_KEY}/photos?page=:page`,
+      destination: `https://devc-my-unsplash-api.onrender.com/api/${process.env.API_KEY}/photos?page=:page`,
     },
     {
       source: "/photos/:label/:page",
-      destination: `http://localhost:5000/api/${process.env.API_KEY}/photos?label=:label&page=:page`,
+      destination: `https://devc-my-unsplash-api.onrender.com/api/${process.env.API_KEY}/photos?label=:label&page=:page`,
     },
   ]
 }
